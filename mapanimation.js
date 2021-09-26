8400e96549eb44a5159cd2531f8dba9cb29331b9
Simrat Karamjeet <87209581+skaramje@users.noreply.github.com>
2021-09-25 19:31:56 -0600

    Update mapanimation.js

--- mapanimation.js
+++ mapanimation.js
@@ -1,5 +1,5 @@
 
-mapboxgl.accessToken = '';
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2thcmFtamUiLCJhIjoiY2t0bjl5Z2lqMDByNDJvbzN2bmJqODVnayJ9.39REDeKQcsQPv5yV2I0Kfg';
 
 var map = new mapboxgl.Map({
     container: 'map',
